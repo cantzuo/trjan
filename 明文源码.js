965e1f23cc1f6cbc2742fc23a8131d190677ffc8
cantzuo <156641993+cantzuo@users.noreply.github.com>
2025-05-09 09:05:37 +0800

    更新 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from "cloudflare:sockets";
 
 let password = '5583';
-let proxyIP = '[2a01:4f9:6b:47e9::1971]:21743';
+let proxyIP = 'ProxyIP.Oracle.CMLiussss.net';
 let sub = 'wtr.sosorg.nyc.mn'; 
 let subConverter = 'SUBAPI.fxxk.dedyn.io';// clash订阅转换后端，目前使用CM的订阅转换功能。自带虚假节点信息防泄露
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"; //订阅配置文件
